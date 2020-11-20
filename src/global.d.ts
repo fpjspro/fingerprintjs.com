95b2e60cee5d6cb02fb6142fdd7d940e9a99db03
Boris Lobanov <borislobanov92@gmail.com>
2020-11-20 21:14:52 +0300

    added type declaration for png

--- src/global.d.ts
+++ src/global.d.ts
@@ -5,6 +5,8 @@ declare module '*.svg' {
   export default src
 }
 
+declare module '*.png'
+
 declare module '*.module.scss' {
   const classes: { readonly [key: string]: string }
   export default classes
