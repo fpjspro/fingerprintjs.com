3e1ccd3be2153af25dceb7a86d5827ffe7a8a373
Boris Lobanov <borislobanov92@gmail.com>
2020-11-25 20:34:33 +0300

    hero page mvp

---
+++ content/pages/hero-page.md
@@ -0,0 +1,6 @@
+---
+templateKey: hero-page
+title: When are you going to click that button?
+description: You know you are interested in our products and services but when will you take the next step? Sign Up and get instant updates delivered right to your inbox. 
+ctaText: Save me!
+---
--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,5 +1,7 @@
 import CMS from 'netlify-cms-app'
 
 import { AccountSharingPagePreview } from '../templates/account-sharing-page'
+import { HeroPagePreview } from '../templates/hero-page'
 
 CMS.registerPreviewTemplate('account-sharing-page', AccountSharingPagePreview)
+CMS.registerPreviewTemplate('hero-page', HeroPagePreview)
