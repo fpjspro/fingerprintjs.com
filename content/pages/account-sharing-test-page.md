caf9306f73b0332f5b966e68d271b594adcdb452
Boris Lobanov <borislobanov92@gmail.com>
2020-11-26 16:48:57 +0300

    feat: alternating image component

--- content/pages/account-sharing-test-page.md
+++ content/pages/account-sharing-test-page.md
@@ -2,10 +2,19 @@
 templateKey: account-sharing-page
 title: Dogs are great!
 block1:
-  subheader: Dog's parts (old)
+  subheader: All of our plans include 24/7 emailing and our full suite of features
   bullets:
-    - paws
-    - tail
-    - boop
+    - First feature
+    - Second feature with a longer name
+    - Third feature
   image: /img/uploads/dog.jpg
+  imageAfterText: false
+block2:
+  subheader: All of our plans include 24/7 emailing and our full suite of features
+  bullets:
+    - First feature
+    - Second feature with a longer name
+    - Third feature
+  image: /img/uploads/cat.jpg
+  imageAfterText: true
 ---
--- src/components/widgets/AlternatingImagesText/index.tsx
+++ src/components/widgets/AlternatingImagesText/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import PreviewCompatibleImage, { ImageInfo } from '../../common/PreviewCompatibleImage/PreviewCompatibleImage'
 import TextList from '../../common/TextList'
-
+import Container from '../../common/Container'
+import Section from '../../common/Section'
+import classNames from 'classnames'
 import styles from './AlternatingImagesText.module.scss'
 
 export interface BlockWithImage {
   image: ImageInfo
   subTitle: string
   bullets: string[]
+  imageAfterText: boolean
 }
 
 export interface AlternatingImagesTextProps {
@@ -16,26 +19,26 @@ export interface AlternatingImagesTextProps {
 }
 export default function AlternatingImagesText({ title, blocks }: AlternatingImagesTextProps) {
   return (
-    <section className={styles.root}>
+    <Section className={styles.root}>
       <h2 className={styles.title}>{title}</h2>
 
       {blocks.map((block) => (
         <Block key={block.subTitle} block={block} />
       ))}
-    </section>
+    </Section>
   )
 }
 
 function Block({ block }: { block: BlockWithImage }) {
   return (
-    <section className={styles.block}>
+    <Container className={styles.container}>
       <PreviewCompatibleImage className={styles.image} imageInfo={block.image} />
 
-      <div className={styles.text}>
+      <div className={classNames(styles.text, { [styles.left]: block.imageAfterText })}>
         <h3 className={styles.blockTitle}>{block.subTitle}</h3>
         <TextList items={block.bullets} ordered />
       </div>
-    </section>
+    </Container>
   )
 }
 
