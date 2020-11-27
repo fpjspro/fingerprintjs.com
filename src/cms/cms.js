21d1a04ec0a6b3820b98a4eb2a4f5a408834012f
Boris Lobanov <borislobanov92@gmail.com>
2020-11-27 17:52:16 +0300

    removed hero page from preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,7 +1,5 @@
 import CMS from 'netlify-cms-app'
 
 import { AccountSharingPagePreview } from '../templates/account-sharing-page'
-import { HeroPagePreview } from '../templates/hero-page'
 
 CMS.registerPreviewTemplate('account-sharing-page', AccountSharingPagePreview)
-CMS.registerPreviewTemplate('hero-page', HeroPagePreview)
