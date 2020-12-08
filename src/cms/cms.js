0f3770917e0b12b98a1021b3edb3e96647693342
tan-90 <tan-90@outlook.com>
2020-12-08 15:25:22 -0300

    Fixed CMS preview not working.

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,4 +2,4 @@ import CMS from 'netlify-cms-app'
 
 import { StaticPageContentPreview } from '../templates/static-page-content'
 
-CMS.registerPreviewTemplate('static-page-content', StaticPageContentPreview)
+CMS.registerPreviewTemplate('index', StaticPageContentPreview)
