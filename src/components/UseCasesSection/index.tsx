64b8a32efb67a4b557c1fc4375d27f2517c2b876
tan-90 <tan-90@outlook.com>
2020-12-10 15:05:22 -0300

    Extracted use case cards into components.
    Added links to static pages that match use cases.

--- src/components/UseCasesSection/index.tsx
+++ src/components/UseCasesSection/index.tsx
@@ -29,86 +29,70 @@ export default function UseCasesSection() {
         </header>
         <div className={styles.content}>
           <div className={styles.useCases}>
-            <div className={classNames(styles.useCase, styles.large)}>
-              <div className={styles.iconContainer}>
-                <AccountFraudSvg className={styles.icon} />
-              </div>
-              <div className={styles.info}>
-                <h3 className={styles.title}>Account Fraud</h3>
-                <p className={styles.description}>
-                  Confirm that every visitor on your website is real and not an advanced bot using multiple techniques
-                  to create fake accounts.
-                  <br />
-                  <br />
-                  You can mitigate account takeover attempts, prevent password sharing and significantly reduce the
-                  number of fake accounts.
-                </p>
-              </div>
-            </div>
-            <div className={classNames(styles.useCase, styles.large)}>
-              <div className={styles.iconContainer}>
-                <PaymentProcessingSvg className={styles.icon} />
-              </div>
-              <div className={styles.info}>
-                <h3 className={styles.title}>Payment Processing</h3>
-                <p className={styles.description}>
-                  Identify anonymous visitors behind every transaction. Instantly recognize repeated card testing
-                  activity and link it to specific users.
-                  <br />
-                  <br />
-                  Significantly reduce chargebacks and fraudulent payments just one month after integrating
-                  FingerprintJS on your website.
-                </p>
-              </div>
-            </div>
-            <Link to='ecommerce' className={styles.useCase}>
-              <div className={styles.iconContainer}>
-                <ECommerceSvg className={styles.icon} />
-              </div>
-              <div className={styles.info}>
-                <h3 className={styles.title}>E-Commerce</h3>
-                <p className={styles.description}>
-                  Every fraudulent order is money directly out of your pocket. With our best in class tools you can stop
-                  malicious users before they cost you real money.
-                </p>
-              </div>
-            </Link>
-            <div className={styles.useCase}>
-              <div className={styles.iconContainer}>
-                <CryptoCurrencySvg className={styles.icon} />
-              </div>
-              <div className={styles.info}>
-                <h3 className={styles.title}>Cryptocurrency</h3>
-                <p className={styles.description}>
-                  Ensure that your trading, exchange and transfer operations are safe from malicious activity or account
-                  fraud.
-                </p>
-              </div>
-            </div>
-            <div className={styles.useCase}>
-              <div className={styles.iconContainer}>
-                <GamingSvg className={styles.icon} />
-              </div>
-              <div className={styles.info}>
-                <h3 className={styles.title}>Gaming</h3>
-                <p className={styles.description}>
-                  Catch users trying to break your system via multiple accounts, devices, and IP addresses to unjustly
-                  enrich themselves.
-                </p>
-              </div>
-            </div>
-            <div className={styles.useCase}>
-              <div className={styles.iconContainer}>
-                <CustomSolutionSvg className={styles.icon} />
-              </div>
-              <div className={styles.info}>
-                <h3 className={styles.title}>Custom Solution</h3>
-                <p className={styles.description}>We can build a custom solution that works for your industry.</p>
-              </div>
-            </div>
+            <UseCase iconComponent={AccountFraudSvg} title='Account Fraud' large>
+              Confirm that every visitor on your website is real and not an advanced bot using multiple techniques to
+              create fake accounts.
+              <br />
+              <br />
+              You can mitigate account takeover attempts, prevent password sharing and significantly reduce the number
+              of fake accounts.
+            </UseCase>
+            <UseCase link='payment-fraud' iconComponent={PaymentProcessingSvg} title='Payment Processing' large>
+              Identify anonymous visitors behind every transaction. Instantly recognize repeated card testing activity
+              and link it to specific users.
+              <br />
+              <br />
+              Significantly reduce chargebacks and fraudulent payments just one month after integrating FingerprintJS on
+              your website.
+            </UseCase>
+            <UseCase link='ecommerce' iconComponent={ECommerceSvg} title='E-Commerce'>
+              Every fraudulent order is money directly out of your pocket. With our best in class tools you can stop
+              malicious users before they cost you real money.
+            </UseCase>
+            <UseCase link='cryptocurrency' iconComponent={CryptoCurrencySvg} title='Cryptocurrency'>
+              Ensure that your trading, exchange and transfer operations are safe from malicious activity or account
+              fraud.
+            </UseCase>
+            <UseCase link='gaming' iconComponent={GamingSvg} title='Gaming'>
+              Catch users trying to break your system via multiple accounts, devices, and IP addresses to unjustly
+              enrich themselves.
+            </UseCase>
+            <UseCase iconComponent={CustomSolutionSvg} title='Custom Solution'>
+              We can build a custom solution that works for your industry.
+            </UseCase>
           </div>
         </div>
       </Container>
     </Section>
+  )
+}
+
+interface UseCaseProps {
+  link?: string
+  iconComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+  title: string
+  children: React.ReactNode
+  large?: boolean
+}
+function UseCase({ link, iconComponent, title, children, large }: UseCaseProps) {
+  const Icon = iconComponent
+  const content = (
+    <>
+      <div className={styles.iconContainer}>
+        <Icon className={styles.icon} />
+      </div>
+      <div className={styles.info}>
+        <h3 className={styles.title}>{title}</h3>
+        <p className={styles.description}>{children}</p>
+      </div>
+    </>
+  )
+
+  return link ? (
+    <Link to={link} className={classNames(styles.useCase, { [styles.large]: large })}>
+      {content}
+    </Link>
+  ) : (
+    <div className={classNames(styles.useCase, { [styles.large]: large })}>{content}</div>
   )
 }
