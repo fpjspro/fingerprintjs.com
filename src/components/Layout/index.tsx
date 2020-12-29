d92105fd79e205878d55487d02872c62696c49bd
Dmitry Ivanov <liseniss@gmail.com>
2020-12-29 20:45:53 +0300

    fix: try to fix metadata

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -46,10 +46,10 @@ export function LayoutTemplate({ children, siteMetadata }: LayoutTemplateProps) 
         <meta property='twitter:title' content={title} />
         <meta property='twitter:description' content={description} />
         <meta property='twitter:image' content={image} />
-        <script src={`https://www.googleoptimize.com/optimize.js?id=${optimizeToken}`} />
         <script>
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','${gtmToken}');`}
         </script>
+        <script src={`https://www.googleoptimize.com/optimize.js?id=${optimizeToken}`} />
         <link
           href='https://fonts.googleapis.com/css2?family=Fira+Mono:wght@400;500;700&family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
           rel='stylesheet'
