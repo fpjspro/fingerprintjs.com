e19812ca5c4a9cd42a34e999016baf7c12633934
tan-90 <tan-90@outlook.com>
2021-01-12 10:28:20 -0300

    Added context-based environment variables.

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,4 +1,17 @@
-export const FPJS_TOKEN = process.env.GATSBY_FPJS_TOKEN ?? 'test_client_token'
+type EnvironmentVariable = string | number | boolean | undefined
+
+enum NetlifyContext {
+  Production,
+  DeployPreview,
+  BranchDeploy,
+}
+const context: NetlifyContext = getNetlifyContext()
+
+export const FPJS_TOKEN =
+  getContextEnv({
+    [NetlifyContext.Production]: process.env.GATSBY_FPJS_TOKEN,
+    [NetlifyContext.DeployPreview]: process.env.GATSBY_PREVIEW_FPJS_TOKEN,
+  }) ?? 'test_client_token'
 export const FPJS_API_TOKEN = process.env.GATSBY_FPJS_API_TOKEN ?? 'test_fpjs_api_token'
 export const FPJS_ENDPOINT = process.env.GATSBY_FPJS_ENDPOINT ?? ''
 export const FPJS_REGION = process.env.GATSBY_FPJS_REGION
@@ -12,3 +25,20 @@ export const MAPBOX_ACCESS_TOKEN = process.env.GATSBY_MAPBOX_ACCESS_TOKEN
 export const ROLLBAR_ACCESS_TOKEN = process.env.GATSBY_ROLLBAR_ACCESS_TOKEN
 export const FPJS_LEAD_URL = process.env.GATSBY_FPJS_LEAD_URL ?? ''
 export const BRANCH = process.env.BRANCH
+
+function getNetlifyContext(): NetlifyContext {
+  switch (process.env.CONTEXT) {
+    case 'production':
+      return NetlifyContext.Production
+    case 'deploy-preview':
+      return NetlifyContext.DeployPreview
+    case 'branch-deploy':
+      return NetlifyContext.BranchDeploy
+    default:
+      return NetlifyContext.Production
+  }
+}
+
+function getContextEnv(entries: { [key in NetlifyContext]?: EnvironmentVariable }) {
+  return entries[context] ?? entries[NetlifyContext.Production]
+}
