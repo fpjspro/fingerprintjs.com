dc7e2edeaddd661bd7a23bacf4b7df9f40f08519
tan-90 <tan-90@outlook.com>
2021-02-10 11:08:53 -0300

    Added URL list to lighthouse config.

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,15 @@
 module.exports = {
   ci: {
+    collect: {
+      url: [
+        'http://localhost/index.html',
+        'http://localhost/demo/index.html',
+        'http://localhost/pricing/index.html',
+        'http://localhost/case-studies/edtech/index.html',
+        'http://localhost/blog/device-fingerprinting-android/index.html',
+        'http://localhost/ecommerce/index.html',
+      ],
+    },
     upload: {
       target: 'temporary-public-storage',
     },
