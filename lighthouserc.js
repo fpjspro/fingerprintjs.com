e05c9bffe6b9b79da1c2362fd7dd7337621892dd
tan-90 <tan-90@outlook.com>
2021-02-10 12:03:41 -0300

    Removed extra pages from lighthouse checks.

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,14 +2,7 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: './public',
-      url: [
-        'http://localhost/index.html',
-        'http://localhost/demo/index.html',
-        'http://localhost/pricing/index.html',
-        'http://localhost/case-studies/edtech/index.html',
-        'http://localhost/blog/device-fingerprinting-android/index.html',
-        'http://localhost/ecommerce/index.html',
-      ],
+      url: ['http://localhost/index.html', 'http://localhost/demo/index.html', 'http://localhost/pricing/index.html'],
     },
     upload: {
       target: 'temporary-public-storage',
