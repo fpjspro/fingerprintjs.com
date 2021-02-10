57f7b6397c8cfd20aadd874e60cadfebeb7d7dc2
tan-90 <tan-90@outlook.com>
2021-02-10 13:10:55 -0300

    Changed lighthouse page list.

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,13 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: './public',
-      url: ['http://localhost/index.html', 'http://localhost/demo/index.html', 'http://localhost/pricing/index.html'],
+      url: [
+        'http://localhost/index.html',
+        'http://localhost/demo/index.html',
+        'http://localhost/pricing/index.html',
+        'http://localhost/case-studies/edtech/index.html',
+        'http://localhost/ecommerce/index.html',
+      ],
     },
     upload: {
       target: 'temporary-public-storage',
