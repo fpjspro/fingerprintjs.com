ee10618950277f9d4d7b624c84c50d38f40e030f
natapg <natanael.galfrascoli@fingerprintjs.com>
2021-03-15 17:13:31 -0300

    changed image per grid

--- src/components/githubPage/ProLayersSection/ProLayersSection.tsx
+++ src/components/githubPage/ProLayersSection/ProLayersSection.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Section from '../../common/Section'
 import Container from '../../common/Container'
-import proLayers from './proLayers.png'
+import proLayers from './proLayersArrows.png'
 
 import styles from './ProLayersSection.module.scss'
 
@@ -16,7 +16,29 @@ export default function ProLayersSection() {
             learning to generate stable and highly accurate visitorIDs for every web visitor.
           </h2>
         </header>
-        <img alt='FingerprintJS Pro layers' src={proLayers} className={styles.image} />
+        <div className={styles.wrapper}>
+          <div className={styles.imageWrapper}>
+            <div className={styles.aside}>
+              <div>Runs on server</div>
+              <div>Runs in browser</div>
+            </div>
+            <img alt='FingerprintJS Pro layers' src={proLayers} className={styles.image} />
+          </div>
+          <div className={styles.textWrapper}>
+            <div>
+              <h1 className={styles.layerTitle}>Server-side analysis and machine learning</h1>
+            </div>
+            <div>
+              <h3 className={styles.layerTitle}>Storage and deduplication</h3>
+            </div>
+            <div>
+              <h3 className={styles.layerTitle}>Cookie and local storage management</h3>
+            </div>
+            <div>
+              <h3 className={styles.layerTitle}>Browser Fingerprinting Library</h3>
+            </div>
+          </div>
+        </div>
       </Container>
     </Section>
   )
