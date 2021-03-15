cf0fed97f6f9827f4fbd24ff4c55526a3fdc4cef
natapg <natanael.galfrascoli@fingerprintjs.com>
2021-03-15 12:18:17 -0300

    added subtitle

--- src/components/githubPage/ProLayersSection/ProLayersSection.tsx
+++ src/components/githubPage/ProLayersSection/ProLayersSection.tsx
@@ -8,10 +8,14 @@ import styles from './ProLayersSection.module.scss'
 export default function ProLayersSection() {
   return (
     <Section className={styles.root}>
-      <Container size='large' className={styles.container}>
-        <div>
-          <h2 className={styles.title}>Pro is Powered By Open Source</h2>
-        </div>
+      <Container className={styles.container}>
+        <header className={styles.header}>
+          <h1 className={styles.title}>Pro is Powered By Open Source</h1>
+          <h2 className={styles.subtitle}>
+            FingerprintJS Pro introduces server-side identification techniques, deduplication analysis, and machine
+            learning to generate stable and highly accurate visitorIDs for every web visitor.
+          </h2>
+        </header>
         <img alt='FingerprintJS Pro layers' src={proLayers} className={styles.image} />
       </Container>
     </Section>
