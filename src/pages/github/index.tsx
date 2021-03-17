cfba21c9e28479e35735582bea0ef309120924e2
natapg <natanael.galfrascoli@fingerprintjs.com>
2021-03-17 16:20:52 -0300

    added whygopro section

---
+++ src/pages/github/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { LayoutTemplate } from '../../components/Layout'
+import BreadcrumbsSEO from '../../components/Breadcrumbs/BreadcrumbsSEO'
+import { GeneratedPageContext } from '../../helpers/types'
+
+import WhyGoProSection from '../../components/githubPage/WhyGoProSection/WhyGoProSection'
+import useSiteMetadata from '../../hooks/useSiteMetadata'
+import { useLocation } from '@reach/router'
+
+interface GitHubProps {
+  pageContext: GeneratedPageContext
+}
+export default function GitHubPage({ pageContext }: GitHubProps) {
+  const breadcrumbs = pageContext.breadcrumb.crumbs
+  const { pathname } = useLocation()
+  let siteMetadata = useSiteMetadata()
+  siteMetadata = {
+    ...siteMetadata,
+    title: 'FingerprintJS Pro vs Open Source - Visitor Identification API Comparison',
+    siteUrl: `${siteMetadata.siteUrl}${pathname}`,
+  }
+
+  return (
+    <LayoutTemplate siteMetadata={siteMetadata}>
+      {breadcrumbs && <BreadcrumbsSEO breadcrumbs={breadcrumbs} />}
+
+      <WhyGoProSection />
+    </LayoutTemplate>
+  )
+}
