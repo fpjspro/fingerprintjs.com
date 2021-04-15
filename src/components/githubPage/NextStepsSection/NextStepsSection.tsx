28e30652dd76c291ead0fabf56ab2c1868f3d3a2
natapg <natanael.galfrascoli@fingerprintjs.com>
2021-04-14 21:25:45 -0300

    added mobile view

--- src/components/githubPage/NextStepsSection/NextStepsSection.tsx
+++ src/components/githubPage/NextStepsSection/NextStepsSection.tsx
@@ -1,36 +1,52 @@
 import React from 'react'
 import Section from '../../common/Section'
 import Container from '../../common/Container'
-import Button from '../../common/Button'
-import { PATH } from '../../../constants/content'
+import { URL, DOC_URL, MAILTO } from '../../../constants/content'
 
 import styles from './NextStepsSection.module.scss'
 
 export default function NextStepsSection() {
   return (
     <Section className={styles.root}>
-      <Container className={styles.container}>
-        <h1 className={styles.title}>Next Steps with FingerprintJS Pro</h1>
+      <Container className={styles.container} size='large'>
+        <header className={styles.sectionHeader}>
+          <h1 className={styles.title}>Next Steps with FingerprintJS Pro</h1>
+        </header>
+        <div className={styles.stepsSection}>
+          <Step step='Sign up for a free account' url={URL.dashboardLoginUrl} linkText='Create account today >'>
+            Sign up for free for up to 1,000 unique monthly visitors.
+          </Step>
+          <Step step='Read our docs' url={DOC_URL.documentationUrl} linkText='View docs >'>
+            Learn how to implement FingerprintJS Pro on your website and start collecting VisitorIDs.
+          </Step>
+          <Step step='Reach out with questions' url={MAILTO.mailToUrl} linkText='Contact support >'>
+            We would love to help out! Reach out to support via email or chat anytime.
+          </Step>
+          <Step step='Subscribe to our newsletter' url={URL.newsletterUrl} linkText='Sign up for updates >'>
+            Stay up to date with the latest in browser fingerprinting and user identification news.
+          </Step>
+        </div>
       </Container>
     </Section>
   )
 }
 
 interface StepProps {
-  title: string
+  step: string
   children: string
-  link: string
+  url: string
+  linkText: string
 }
-function Step({ title, children, link }: StepProps) {
+function Step({ step, children, url, linkText }: StepProps) {
   return (
     <>
-      <div>
-        <td className={styles.featureColumn}>
-          <strong>{feature}</strong>
-          {children && <div className={styles.description}>{children}</div>}
-        </td>
-        <td className={classNames(styles.versionColumn, { [styles.versionColumnStrong]: isSymbol })}>{oss}</td>
-        <td className={classNames(styles.versionColumn, { [styles.versionColumnStrong]: isSymbol })}>{pro}</td>
+      <div className={styles.stepCard}>
+        <div className={styles.stepNumber} />
+        <h1 className={styles.stepTitle}>{step}</h1>
+        <p className={styles.stepDescription}>{children}</p>
+        <a className={styles.stepLink} href={url} target='_blank' rel='noreferrer'>
+          {linkText}
+        </a>
       </div>
     </>
   )
