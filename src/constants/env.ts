6b30da7d3cd56e09f1f7d61cb79373e27dad70bf
Dmitry Ivanov <liseniss@gmail.com>
2021-05-31 18:44:59 +0300

    fix: add csp header

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -46,6 +46,8 @@ export const ROLLBAR_ACCESS_TOKEN = process.env.GATSBY_ROLLBAR_ACCESS_TOKEN
 export const FPJS_LEAD_URL = process.env.GATSBY_FPJS_LEAD_URL ?? ''
 export const BRANCH = process.env.BRANCH
 
+export const isProdContext = getNetlifyContext() === NetlifyContext.Production
+
 function getNetlifyContext(): NetlifyContext {
   switch (process.env.CONTEXT) {
     case 'production':
